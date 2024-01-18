e240dfe7cb210db807963acd22b3ce070dbb5c75
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-01-17 22:17:38 -0400

    UserService(injecao de UserRepository) e UserController(Post) com ponto de injecao UserService

--- user/src/main/java/com/ms/user/controllers/UserController.java
+++ user/src/main/java/com/ms/user/controllers/UserController.java
@@ -2,8 +2,10 @@ package com.ms.user.controllers;
 
 import com.ms.user.dtos.UserRecordDto;
 import com.ms.user.models.UserModel;
+import com.ms.user.services.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,10 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
+    @Autowired
+    UserService userService;
+
     @PostMapping("/users")
     public ResponseEntity<UserModel> saveUser(@RequestBody @Valid UserRecordDto userRecordDto){
         var userModel = new UserModel();
         BeanUtils.copyProperties(userRecordDto, userModel);
-        return ResponseEntity.status(HttpStatus.CREATED).body(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(userModel));
     }
 }
